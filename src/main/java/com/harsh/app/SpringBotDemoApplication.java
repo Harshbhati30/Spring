f1e34ef3225a2c5c4dec3be5db8d182aebd1f4d5
Harshbhati30 <bhatiharsh9818@gmail.com>
2025-06-20 20:55:30 +0530

    Initial commit for SpringBotDemo

---
+++ src/main/java/com/harsh/app/SpringBotDemoApplication.java
@@ -0,0 +1,16 @@
+package com.harsh.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class SpringBotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBotDemoApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/harsh/app/SpringBotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
