07bfd35ac3c30764bcebe4366db0abdad1b1f864
Harshbhati30 <bhatiharsh9818@gmail.com>
2025-06-21 10:55:28 +0530

    Setter Injecrtions

--- Spring1/src/main/java/com/harsh/Alien.java
+++ Spring1/src/main/java/com/harsh/Alien.java
@@ -3,11 +3,19 @@ package com.harsh;
 import java.sql.SQLOutput;
 
 public class Alien {
-    int age;
-
+    private int age;
     public Alien(){
         System.out.println("Aline Object");
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void code(){
         System.out.println("Coding0...");
     }
