62c2258f8028f06f1c1c3d4854f5fb77b3849a06
Harshbhati30 <bhatiharsh9818@gmail.com>
2025-06-21 17:32:16 +0530

    GetBean By Type

--- Spring1/src/main/java/com/harsh/App.java
+++ Spring1/src/main/java/com/harsh/App.java
@@ -12,9 +12,17 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");  //With this line we created the container and in double qotes it has containers configuration file address
-        Alien a= (Alien) context.getBean("alien");
+//        Alien a= (Alien) context.getBean("alien");  OR
+
+//        Alien a=context.getBean(Alien.class);  //Or
+
+        Alien a=context.getBean("alien",Alien.class);
+
+
         System.out.println(a.getAge());
         a.code();
+
+        Computer c=context.getBean(Desktop.class);
 
     }
 }
