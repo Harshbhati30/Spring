9d91c96b7394ca23965524d561a42ec1082a8878
Harshbhati30 <bhatiharsh9818@gmail.com>
2025-06-21 17:39:23 +0530

    Java Based Configration

--- Spring1/src/main/java/com/harsh/App.java
+++ Spring1/src/main/java/com/harsh/App.java
@@ -1,6 +1,8 @@
 package com.harsh;
 
+import com.harsh.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -11,18 +13,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");  //With this line we created the container and in double qotes it has containers configuration file address
-//        Alien a= (Alien) context.getBean("alien");  OR
-
-//        Alien a=context.getBean(Alien.class);  //Or
-
-        Alien a=context.getBean("alien",Alien.class);
+        ApplicationContext context =new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop dt=context.getBean(Desktop.class);
+        dt.compile();
 
 
-        System.out.println(a.getAge());
-        a.code();
 
-        Computer c=context.getBean(Desktop.class);
+
+
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");  //With this line we created the container and in double qotes it has containers configuration file address
+////        Alien a= (Alien) context.getBean("alien");  OR
+//
+////        Alien a=context.getBean(Alien.class);  //Or
+//
+//        Alien a=context.getBean("alien",Alien.class);
+//
+//
+//        System.out.println(a.getAge());
+//        a.code();
+//
+//        Computer c=context.getBean(Desktop.class);
 
     }
 }
