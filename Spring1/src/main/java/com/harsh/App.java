988f2c848598aba1a6fdfcf3a1c9eb89f0b3f5a2
Harshbhati30 <bhatiharsh9818@gmail.com>
2025-06-22 11:00:56 +0530

    Scope,Autowire -Java Based Configuration

--- Spring1/src/main/java/com/harsh/App.java
+++ Spring1/src/main/java/com/harsh/App.java
@@ -14,8 +14,16 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context =new AnnotationConfigApplicationContext(AppConfig.class);
+
+//        Desktop dt=context.getBean("desktop", Desktop.class);  OR
         Desktop dt=context.getBean(Desktop.class);
         dt.compile();
+
+        Alien a=context.getBean(Alien.class);
+        System.out.println(a.getAge());
+        a.code();
+
+
 
 
 
