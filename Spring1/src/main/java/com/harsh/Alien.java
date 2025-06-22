d9582653b4afa490ec1bfe6a8dac7247f832d7c0
Harshbhati30 <bhatiharsh9818@gmail.com>
2025-06-22 11:10:33 +0530

    Scope,Autowire -Java Based Configuration

--- Spring1/src/main/java/com/harsh/Alien.java
+++ Spring1/src/main/java/com/harsh/Alien.java
@@ -1,8 +1,17 @@
 package com.harsh;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 import java.sql.SQLOutput;
 
+@Component
 public class Alien {
+
+    @Value("26")
     private int age;
     private Computer comp;
 
@@ -18,6 +27,7 @@ public class Alien {
         return comp;
     }
 
+    @Autowired
     public void setComp(Computer comp) {
         this.comp = comp;
     }
--- Spring1/src/main/java/com/harsh/config/AppConfig.java
+++ Spring1/src/main/java/com/harsh/config/AppConfig.java
@@ -5,28 +5,30 @@ import com.harsh.Computer;
 import com.harsh.Desktop;
 import com.harsh.Laptop;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan("com.harsh")
 public class AppConfig {
 
-    @Bean(name ="desktop")
-    public Desktop desktop(){
-        return new Desktop();
-    }
-
-    @Bean
-    @Primary
-    public Laptop laptop(Computer comp){
-        return new Laptop();
-    }
-
-    @Bean
-    public Alien alien(Computer comp){
-        Alien obj=new Alien();
-        obj.setAge(32);
-        obj.setComp(comp);
-        return obj;
-    }
+//    @Bean(name ="desktop")
+//    public Desktop desktop(){
+//        return new Desktop();
+//    }
+//
+//    @Bean
+//    @Primary
+//    public Laptop laptop(Computer comp){
+//        return new Laptop();
+//    }
+//
+//    @Bean
+//    public Alien alien(Computer comp){
+//        Alien obj=new Alien();
+//        obj.setAge(32);
+//        obj.setComp(comp);
+//        return obj;
+//    }
 }
